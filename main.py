d5a414b01b5f38fbb626333a371bce67304100e7
nyteflyte <hshultz00@gmail.com>
2025-07-09 10:32:03 -0500

    text analysis works, but the code ugly.

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-    
+from stats import count_words
+from stats import count_chars
+from stats import convert_to_list    
+from stats import sort_on
+
+book_path = "books/frankenstein.txt"
+
 def get_book_text():
-    with open("./books/frankenstein.txt") as f:
+    with open(book_path) as f:
         file_contents = f.read()
     return file_contents
 
-def count_words(file_contents):
-    string = file_contents.split()
-    num_words = len(string)
-    return num_words
+def print_pretty(book_path,book_length,ordered_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {book_length} total words")
+    print("--------- Character Count -------")
+    for i in ordered_chars:
+        char = i['char']
+        num = i['num']
+        print (f"{char}: {num}")
+
 
 def main():
     book_text = get_book_text()
     book_length = count_words(book_text)
-    print(f"{book_length} words found in the document")
-
+    char_count = count_chars(book_text)
+    char_list = convert_to_list(char_count)
+    ordered_chars = [char_list]
+    ordered_chars.sort(reverse=True, key=sort_on)
+    ordered_chars = ordered_chars[0]
+    print_pretty(book_path,book_length,ordered_chars)
 main()
---
+++ stats.py
@@ -0,0 +1,31 @@
+def count_words(file_contents):
+    string = file_contents.split()
+    num_words = len(string)
+    return num_words
+
+def count_chars(file_contents):
+    char_count = {}
+    lower_case = file_contents.lower()
+
+    for character in lower_case:
+        if character.isalpha():
+            if character not in char_count:
+                char_count[character] = 1
+            else:
+                char_count[character] += 1
+    return char_count
+
+def convert_to_list(char_count):
+    list_of_chars = []
+    for char in char_count:
+        value = char_count[char]
+        char_dict = {
+            "char" : char,
+            "num" : value
+        }
+        list_of_chars.append(char_dict)
+    return list_of_chars
+
+def sort_on(char_list):
+    for i in char_list:
+        return i["num"]
